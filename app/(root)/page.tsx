74886039cf540f938e4d54eb89e28017093f4349
Tazim <sayeeditt@gmail.com>
2025-12-23 00:36:34 +0600

    code cleared

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,17 +2,8 @@ import PopularDest from "../components/PopularDest";
 import Services from "../components/Services";
 import TourPackages from "../components/TourPackages";
 import TravelNext from "../components/TravekNext";
-// import { waPhoneNumber } from "./../utils/constants";
 
 const page = () => {
-  // const message = "Hello, I would like to know more about your services.";
-
-  // const url = `https://wa.me/${waPhoneNumber}?text=${encodeURIComponent(
-  //   message
-  // )}`;
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-
   return (
     <div>
       <Services />
@@ -20,15 +11,6 @@ const page = () => {
       <TravelNext />
       <TourPackages />
     </div>
-
-    //   <a
-    //     className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-39.5"
-    //     href={`${url}`}
-    //     target="_blank"
-    //     rel="noopener noreferrer"
-    //   >
-    //     Documentation
-    //   </a>
   );
 };
 
