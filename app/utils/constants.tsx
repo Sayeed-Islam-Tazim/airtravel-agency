7b266c4a15a4480d78e0a0af1478e9aee0387c1b
Tazim <sayeeditt@gmail.com>
2025-12-23 00:39:11 +0600

    constant changed

--- app/utils/constants.tsx
+++ app/utils/constants.tsx
@@ -1,4 +1,4 @@
-export const waPhoneNumber = "01303023378";
+export const waPhoneNumber = "8801303023378";
 export const mailAddress = "info@airtravelagency.com";
 export const physicalAddress =
   "333/1-B, Ekora Road, Ahmednagar, Paikpara, Mirpur-1, Dhaka-1216";
